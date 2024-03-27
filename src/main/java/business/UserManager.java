d725cd33a3e3307c86ae4a75b357cdfa1582e39d
Mercy <d00251752@student.dkit.ie>
2024-03-27 01:42:33 +0000

    add javaDoc in UserManager class

--- src/main/java/business/UserManager.java
+++ src/main/java/business/UserManager.java
@@ -7,6 +7,13 @@ public class UserManager {
 
     private final ArrayList<User> usersList = new ArrayList<User>();
 
+    /**
+     * Add a new user to the list of usersList.
+     * If a user already exists matching the username and password in usersList, new user won't be added.
+     * @param username username of the user
+     * @param password password of the user
+     * @return true if the user was successfully added, else return false.
+     */
     public boolean addUser(String username, String password)
     {
         User user = new User(username,password);
@@ -19,6 +26,11 @@ public class UserManager {
         }
     }
 
+    /**
+     * Search user by username that store in usersList.
+     * @param username username of the user we want to search.
+     * @return a arrayList of all user by the specified username.
+     */
     public ArrayList<User> searchByUsername(String username)
     {
         ArrayList<User> matches = new ArrayList<User>();
